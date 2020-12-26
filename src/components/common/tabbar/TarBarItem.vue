b491cf6da8d2548793175e0829ab220b3f109c42
kz <2667422922@qq.com>
2020-12-26 16:35:18 +0800

    '初始化'

---
+++ src/components/common/tabbar/TarBarItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="tab-bar-item" @click="itemclick">
+    <div v-if="!isActive">
+      <slot name='item-active'></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon"></slot>
+    </div>
+    <div :class="{active:isActive}">
+      <slot name="item-text"></slot>
+    </div>
+    <!--    <img src="../../assets/img/tabbar/分享.png"></img>-->
+    <!--    <div>首页</div>-->
+  </div>
+</template>
+<script>
+export default {
+  name: "TarBarItem",
+  props: {
+    path: String
+  },
+  data() {
+    return {
+      // isActive: true
+    }
+  },
+  computed: {
+    isActive() {
+      // /home -> item1(/home) = true
+      // /home -> item1(/category) = false
+      // /home -> item1(/cart) = true
+      // /home -> item1(/profile) = true
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    // activeStyle() {
+    //   return this.isActive ? {color: this.activeColor} : {}
+    // }
+  },
+  methods: {
+    itemclick: function () {
+      this.$router.push(this.path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+}
+
+.active {
+  color: red;
+}
+</style>
---
+++ src/components/content/MainTabbar/MainTabbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <!--    <tab-bar>-->
+    <TabBar>
+      <TarBarItem path="/home">
+        <img slot="item-icon" alt="" src="../../../assets//img/tabbar/home.svg">
+        <img slot="item-active" alt="" src="../../../assets/img/tabbar/home_active.svg">
+        <div slot="item-text">首页</div>
+      </TarBarItem>
+      <TarBarItem path="/category">
+        <img slot="item-icon" alt="" src="../../../assets/img/tabbar/category.svg">
+        <img slot="item-active" alt="" src="../../../assets/img/tabbar/category_active.svg">
+        <div slot="item-text">分类</div>
+      </TarBarItem>
+      <TarBarItem path="/cart">
+        <img slot="item-icon" alt="" src="../../../assets/img/tabbar/shopcart.svg">
+        <img slot="item-active" alt="" src="../../../assets/img/tabbar/shopcart_active.svg">
+        <div slot="item-text">购物车</div>
+      </TarBarItem>
+      <TarBarItem path="/profile">
+        <img slot="item-icon" alt="" src="../../../assets/img/tabbar/profile.svg">
+        <img slot="item-active" alt="" src="../../../assets/img/tabbar/profile_active.svg">
+        <div slot="item-text">我的</div>
+      </TarBarItem>
+    </TabBar>
+  </div>
+</template>
+<script>
+import TabBar from "@/components/common/tabbar/TabBar";
+import TarBarItem from "@/components/common/tabbar/TarBarItem";
+
+export default {
+  name: 'App',
+  components: {
+    TabBar,
+    TarBarItem
+  }
+
+}
+</script>
+<style>
+/*<!--引入样式-->*/
+
+</style>
