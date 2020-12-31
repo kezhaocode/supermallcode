f7ab8498b56e195c909437f5bebbc1f2ac51a669
kz <2667422922@qq.com>
2020-12-31 13:28:13 +0800

    '初始化'

--- src/components/content/goods/goodslistitem.vue
+++ src/components/content/goods/goodslistitem.vue
@@ -19,17 +19,43 @@ export default {
 
 <style scoped>
 .goodslistitem{
-  float: left;
-  width: 43%;
-  height: 270px;
-  margin-left: 10px;
-  margin-right: 10px;
+  padding-bottom: 40px;
+  position: relative;
 }
 .goodslistitem img{
   width: 100%;
-  /*margin:0 0.2rem;*/
+  border-radius: 5px;
 }
 .goodjs {
   font-size: 12px;
+  position: absolute;
+  bottom: 5px;
+  left: 0;
+  right: 0;
+  overflow: hidden;
+  text-align: center;
+}
+.goodjs p{
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-bottom: 3px;
+}
+.goodjs .price{
+  color: var(--color-high-text);
+  margin-right: 20px;
+}
+.goodjs .collect{
+  position: relative;
+
+}
+.goodjs .collect::after{
+  content: '';
+  position: absolute;
+  left: -15px;
+  top: -1px;
+  width: 14px;
+  height: 14px;
+  background: url("../../../assets/img/home/collect.svg") 0 0/14px 14px;
 }
 </style>
